1b5f3749933ab3ed85a6fb6cae2227d8a02f708a
Anna Thomas <anna.thomas@infosum.com>
2020-11-03 18:07:24 +0000

    Reuse a common parameter in two different features

--- cypress/integration/common/parameters.ts
+++ cypress/integration/common/parameters.ts
@@ -1,10 +1,6 @@
-import {
-  defineParameterType,
-  Then,
-  When,
-} from 'cypress-cucumber-preprocessor/steps';
+import { defineParameterType } from 'cypress-cucumber-preprocessor/steps';
 
-const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+export const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 defineParameterType({
   name: 'note',
@@ -16,14 +12,4 @@ defineParameterType({
   name: 'ordinal',
   regexp: /(\d+)(?:st|nd|rd|th)/,
   transformer: (s) => parseInt(s, 10),
-});
-
-let keySound = null;
-
-When('I press the {ordinal} key of my piano', (number) => {
-  keySound = notes[(number - 1) % 7];
-});
-
-Then('I should hear a(n) {note} sound', (note) => {
-  expect(note).to.equal(keySound);
 });
--- cypress/integration/passWithTypescript/keyboard.feature
+++ cypress/integration/passWithTypescript/keyboard.feature
@@ -1,13 +1,13 @@
 @custom-parameters
-Feature: Reusing Parameter Types
+Feature: Custom Parameter Types
 
   As a cucumber cypress plugin which handles Custom Parameter Types
   I want to allow people to add custom parameter types and use them into their step definitions
 
-  Scenario: Play an A on my piano
-    When I press the 1st key of my piano
+  Scenario: Play an A on my keyboard
+    When I press the 1st key of my keyboard
     Then I should hear an A sound
 
-  Scenario: Play an E on my piano
-    When I press the 26th key of my piano
+  Scenario: Play an E on my keyboard
+    When I press the 26th key of my keyboard
     Then I should hear an E sound
